9826fb876c0e5889a9b130174b50314e4819f76a
hassan <kkromah12@gmail.com>
2024-02-27 17:44:03 +0300

    AOP startrted

--- example17/src/main/java/com/example/aspects/LoggerAspect.java
+++ example17/src/main/java/com/example/aspects/LoggerAspect.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,30 +28,42 @@ public class LoggerAspect {
         joinPoint.proceed();
         Instant finish = Instant.now();
         long timeElapsed = Duration.between(start, finish).toMillis();
-        logger.info("Time took to execute the method : "+timeElapsed);
+        logger.info("Time took to execute the method : " + timeElapsed);
         logger.info(joinPoint.getSignature().toString() + " method execution end");
     }
 
-    @Around("@annotation(com.example.interfaces.LogAspect)")
-    public void logWithAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
-        logger.info(joinPoint.toString() + " method execution start");
-        Instant start = Instant.now();
-        joinPoint.proceed();
-        Instant finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
-        logger.info("Time took to execute the method : "+timeElapsed);
-        logger.info(joinPoint.getSignature().toString() + " method execution end");
-    }
-
-    @AfterThrowing(value = "execution(* com.example.services.*.*(..))",throwing = "ex")
+    @AfterThrowing(value = "execution(* com.example.services.*.*(..))", throwing = "ex")
     public void logException(JoinPoint joinPoint, Exception ex) {
-        logger.log(Level.SEVERE,joinPoint.getSignature()+ " An exception thrown with the help of" +
-                " @AfterThrowing which happened due to : "+ex.getMessage());
+        logger.log(Level.SEVERE, joinPoint.getSignature() + "An exception thrown the help of" +
+                " @AfterThrowing happened due to :" + ex.getMessage());
     }
 
-    @AfterReturning(value = "execution(* com.example.services.*.*(..))",returning = "retVal")
-    public void logStatus(JoinPoint joinPoint,Object retVal) {
-        logger.log(Level.INFO,joinPoint.getSignature()+ " Methods successfully processed with the status " +
+    @AfterReturning(value = "execution(* com.example.services.*.*(..))", returning = "retVal")
+    public void logStatus(JoinPoint joinPoint, Objects retVal) {
+        logger.log(Level.INFO, joinPoint.getSignature() + " Method successfully processed with the status " +
                 retVal.toString());
     }
+
+//    @Around("@annotation(com.example.interfaces.LogAspect)")
+//    public void logWithAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+//        logger.info(joinPoint.toString() + " method execution start");
+//        Instant start = Instant.now();
+//        joinPoint.proceed();
+//        Instant finish = Instant.now();
+//        long timeElapsed = Duration.between(start, finish).toMillis();
+//        logger.info("Time took to execute the method : "+timeElapsed);
+//        logger.info(joinPoint.getSignature().toString() + " method execution end");
+//    }
+//
+//    @AfterThrowing(value = "execution(* com.example.services.*.*(..))",throwing = "ex")
+//    public void logException(JoinPoint joinPoint, Exception ex) {
+//        logger.log(Level.SEVERE,joinPoint.getSignature()+ " An exception thrown with the help of" +
+//                " @AfterThrowing which happened due to : "+ex.getMessage());
+//    }
+//
+//    @AfterReturning(value = "execution(* com.example.services.*.*(..))",returning = "retVal")
+//    public void logStatus(JoinPoint joinPoint,Object retVal) {
+//        logger.log(Level.INFO,joinPoint.getSignature()+ " Methods successfully processed with the status " +
+//                retVal.toString());
+//    }
 }
