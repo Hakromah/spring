1c67b21743dbb58b9ca22ac1358ffd2a79b95c31
Hassan <kkromah12@gmail.com>
2024-03-06 00:10:16 +0300

    Spring security added

--- schoollogin_28/src/main/java/com/hskspring/eazyschool/config/ProjectSecurityConfig.java
+++ schoollogin_28/src/main/java/com/hskspring/eazyschool/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.hskspring.eazyschool.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,13 +10,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectSecurityConfig {
     //permit all the paths we have in the application
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((request) -> request.requestMatchers("", "/", "/home").permitAll()
+                .authorizeHttpRequests((request) -> request.requestMatchers("**", "/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
-                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/contact").permitAll()//.authenticated() u will be required to log in to get access
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/about").permitAll()
