993d1e7c4e8335640b3bffbc79fb9605c05ce416
hassan <kkromah12@gmail.com>
2024-03-13 16:47:18 +0300

    edited

---
+++ schoolmsqlaws_35/src/main/java/com/hskspring/eazyschool/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.hskspring.eazyschool.model;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
