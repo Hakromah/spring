73fdbeb092a59253930464abb3c77f874a03aabf
Hassan <kkromah12@gmail.com>
2024-03-21 23:58:10 +0300

    edited

--- schoolmsqlaws_40/src/main/java/com/hskspring/eazyschool/controller/DashboardController.java
+++ schoolmsqlaws_40/src/main/java/com/hskspring/eazyschool/controller/DashboardController.java
@@ -1,7 +1,11 @@
 package com.hskspring.eazyschool.controller;
 
 
+import com.hskspring.eazyschool.model.Person;
+import com.hskspring.eazyschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,24 +15,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DashboardController {
 
-    @RequestMapping("/dashboard")
+   /* @RequestMapping("/dashboard")
     public String displayDashboard(Model model, Authentication authentication) {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
         //throw new RuntimeException("It's been a bad day!!");
         return "dashboard.html";
     }
+    */
 
-//    @Autowired
-//    PersonRepository personRepository;
-//
-//    @RequestMapping("/dashboard")
-//    public String displayDashboard(Model model, Authentication authentication, HttpSession session) {
-//        Person person = personRepository.readByEmail(authentication.getName());
-//        model.addAttribute("username", person.getName());
-//        model.addAttribute("roles", authentication.getAuthorities().toString());
-//        session.setAttribute("loggedInPerson", person);
-//        return "dashboard.html";
-//    }
+    @Autowired
+    PersonRepository personRepository;
+
+    @RequestMapping("/dashboard")
+    public String displayDashboard(Model model, Authentication authentication, HttpSession session) {
+        Person person = personRepository.readByEmail(authentication.getName());
+        model.addAttribute("username", person.getName());
+        model.addAttribute("roles", authentication.getAuthorities().toString());
+        session.setAttribute("loggedInPerson", person);
+        return "dashboard.html";
+    }
 
 }
--- schoolmsqlaws_40/src/main/java/com/hskspring/eazyschool/controller/ProfileController.java
+++ schoolmsqlaws_40/src/main/java/com/hskspring/eazyschool/controller/ProfileController.java
@@ -16,60 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-//@Slf4j
-//@Controller
-//public class ProfileController {
-
-//    @Autowired
-//    PersonRepository personRepository;
-//
-//    @RequestMapping("/displayProfile")
-//    public ModelAndView displayProfileMessages(Model model, HttpSession session) {
-//        // now let load person details from session that we store in dashboard from DB
-//        Person person = (Person) session.getAttribute("loggedInPerson");
-//        Profile profile = new Profile();
-//        // let's populate person profile using person Object that we loaded from the session
-//        profile.setName(person.getName());
-//        profile.setMobileNumber(person.getMobileNumber());
-//        profile.setEmail(person.getEmail());
-//        // let check if address details available in person Object, fetch them as well
-//        if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
-//            profile.setAddress1(person.getAddress().getAddress1());
-//            profile.setAddress2(person.getAddress().getAddress2());
-//            profile.setCity(person.getAddress().getCity());
-//            profile.setState(person.getAddress().getState());
-//            profile.setZipCode(person.getAddress().getZipCode());
-//        }
-//
-//        ModelAndView modelAndView = new ModelAndView("profile.html");
-//        modelAndView.addObject("profile", profile);
-//        return modelAndView;
-//    }
-
-//    @PostMapping(value = "/updateProfile")
-//    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile, Errors errors,
-//                                HttpSession session) {
-//        if (errors.hasErrors()) {
-//            return "profile.html";
-//        }
-//        Person person = (Person) session.getAttribute("loggedInPerson");
-//        person.setName(profile.getName());
-//        person.setEmail(profile.getEmail());
-//        person.setMobileNumber(profile.getMobileNumber());
-//        if (person.getAddress() == null || !(person.getAddress().getAddressId() > 0)) {
-//            person.setAddress(new Address());
-//        }
-//        person.getAddress().setAddress1(profile.getAddress1());
-//        person.getAddress().setAddress2(profile.getAddress2());
-//        person.getAddress().setCity(profile.getCity());
-//        person.getAddress().setState(profile.getState());
-//        person.getAddress().setZipCode(profile.getZipCode());
-//        Person savedPerson = personRepository.save(person);
-//        session.setAttribute("loggedInPerson", savedPerson);
-//        return "redirect:/displayProfile";
-//    }
-//}
-
 @Slf4j
 @Controller
 public class ProfileController {
@@ -78,19 +24,23 @@ public class ProfileController {
     PersonRepository personRepository;
 
     @RequestMapping("/displayProfile")
-    public ModelAndView displayMessages(Model model) {
-//        Person person = (Person) session.getAttribute("loggedInPerson");
+    public ModelAndView displayProfileMessages(Model model, HttpSession session) {
+        // now let load person details from session that we store in dashboard from DB
+        Person person = (Person) session.getAttribute("loggedInPerson");
         Profile profile = new Profile();
-//        profile.setName(person.getName());
-//        profile.setMobileNumber(person.getMobileNumber());
-//        profile.setEmail(person.getEmail());
-//        if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
-//            profile.setAddress1(person.getAddress().getAddress1());
-//            profile.setAddress2(person.getAddress().getAddress2());
-//            profile.setCity(person.getAddress().getCity());
-//            profile.setState(person.getAddress().getState());
-//            profile.setZipCode(person.getAddress().getZipCode());
-//        }
+        // let's populate person profile using person Object that we loaded from the session
+        profile.setName(person.getName());
+        profile.setMobileNumber(person.getMobileNumber());
+        profile.setEmail(person.getEmail());
+        // let check if address details available in person Object, fetch them as well
+        if (person.getAddress() != null && person.getAddress().getAddressId() > 0) {
+            profile.setAddress1(person.getAddress().getAddress1());
+            profile.setAddress2(person.getAddress().getAddress2());
+            profile.setCity(person.getAddress().getCity());
+            profile.setState(person.getAddress().getState());
+            profile.setZipCode(person.getAddress().getZipCode());
+        }
+
         ModelAndView modelAndView = new ModelAndView("profile.html");
         modelAndView.addObject("profile", profile);
         return modelAndView;
