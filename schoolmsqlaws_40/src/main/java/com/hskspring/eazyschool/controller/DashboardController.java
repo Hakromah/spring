c896f66f8668dedf8759939038390189c39745a3
hassan <kkromah12@gmail.com>
2024-03-21 15:59:46 +0300

    edited 21-03-2024

--- schoolmsqlaws_40/src/main/java/com/hskspring/eazyschool/controller/DashboardController.java
+++ schoolmsqlaws_40/src/main/java/com/hskspring/eazyschool/controller/DashboardController.java
@@ -1,11 +1,7 @@
 package com.hskspring.eazyschool.controller;
 
 
-import com.hskspring.eazyschool.model.Person;
-import com.hskspring.eazyschool.repository.PersonRepository;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,15 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DashboardController {
 
-    @Autowired
-    PersonRepository personRepository;
-
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication, HttpSession session) {
-        Person person = personRepository.readByEmail(authentication.getName());
-        model.addAttribute("username", person.getName());
+    public String displayDashboard(Model model, Authentication authentication) {
+        model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
-        session.setAttribute("loggedInPerson", person);
+        //throw new RuntimeException("It's been a bad day!!");
         return "dashboard.html";
     }
+
+//    @Autowired
+//    PersonRepository personRepository;
+//
+//    @RequestMapping("/dashboard")
+//    public String displayDashboard(Model model, Authentication authentication, HttpSession session) {
+//        Person person = personRepository.readByEmail(authentication.getName());
+//        model.addAttribute("username", person.getName());
+//        model.addAttribute("roles", authentication.getAuthorities().toString());
+//        session.setAttribute("loggedInPerson", person);
+//        return "dashboard.html";
+//    }
+
 }
