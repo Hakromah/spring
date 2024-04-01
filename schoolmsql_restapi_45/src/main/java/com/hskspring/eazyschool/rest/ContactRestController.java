a2abf90b177cbfe395c9037b1e4401a31ec0c98b
Hassan <kkromah12@gmail.com>
2024-04-02 02:09:39 +0300

    ContactRestController added

--- schoolmsql_restapi_45/src/main/java/com/hskspring/eazyschool/rest/ContactRestController.java
+++ schoolmsql_restapi_45/src/main/java/com/hskspring/eazyschool/rest/ContactRestController.java
@@ -1,16 +1,23 @@
 package com.hskspring.eazyschool.rest;
 
 import com.hskspring.eazyschool.model.Contact;
+import com.hskspring.eazyschool.model.Response;
 import com.hskspring.eazyschool.repository.ContactRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
-@Controller
+@RestController // = @Controller + @ResponseBody
 @RequestMapping("/api/contact")
 public class ContactRestController {
 
@@ -18,14 +25,14 @@ public class ContactRestController {
     ContactRepository contactRepository;
 
     @GetMapping("/getMessagesByStatus")
-    @ResponseBody
+    //@ResponseBody
     public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
         return contactRepository.findByStatus(status);
     }
 
 
     @GetMapping("/getAllMsgsByStatus")
-    @ResponseBody
+    //@ResponseBody
     public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact) {
         if (null != contact && null != contact.getStatus()) {
 
@@ -33,5 +40,39 @@ public class ContactRestController {
         } else {
             return List.of();
         }
+    }
+
+    // Add message
+    @PostMapping("/saveMsg")
+    // ResponseBody
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom,
+                                            @Valid @RequestBody Contact contact) {
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+    }
+
+    // Delete Message
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) -> {
+            log.info(String.format(
+                    "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
