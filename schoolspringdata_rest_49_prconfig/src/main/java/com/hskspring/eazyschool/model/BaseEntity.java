5d2240dd89fbc858be09837fdbc01321b84f8358
Hassan <kkromah12@gmail.com>
2024-07-12 01:06:05 +0300

    SpringDataJpa class 1 completed

---
+++ schoolspringdata_rest_49_prconfig/src/main/java/com/hskspring/eazyschool/model/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.hskspring.eazyschool.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @JsonIgnore// if you don't want to send any field value along with your Json data to outside
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    @JsonIgnore
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    @JsonIgnore
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    @JsonIgnore
+    private String updatedBy;
+}
